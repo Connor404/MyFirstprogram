5ee5fdac6f03e2ea5a1207febb7745e506162316
Connor Caillet <s198063@students.aie.edu>
2019-08-19 14:59:03 -0500

    Add project files.

---
+++ MyFirstprogram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //output text
+            Console.Write("Hello,what is your name?");
+
+            int playrerscore = 0;
+            string playerName = "Connor";
+
+            int add = 0;
+
+            while (playerscore <10)
+
+                //request input
+                Console.Write("increment score ");
+                Console.Write (Console.ReadLine()):
+                //add score
+                playerscore = playerscore + add
+                console.wright("current score");
+            
+
+            Console.WriteLine("Score of " + playerName);
+            Console.Write("This is my first program. Neat!");
+            Console.ReadLine();
+
+        }
+    }
+}
